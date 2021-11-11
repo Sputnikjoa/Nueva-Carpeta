6fc5f2cc22df84b54ab3a2e25e2638f988100303
Sputnikjoa <joaquin.ros.sed15@gmail.com>
2021-11-11 11:39:28 -0600

    commit media

--- mainescuela/src/mainescuela/Grupo.java
+++ mainescuela/src/mainescuela/Grupo.java
@@ -1,7 +1,11 @@
 
 package mainescuela;
 
+import java.util.ArrayList;
 
 public class Grupo {
     String nombre;
+    Profesor principal;
+    ArrayList <Alumno> listaalumnos;
+    String salon;
 }
--- mainescuela/src/mainescuela/Profesor.java
+++ mainescuela/src/mainescuela/Profesor.java
@@ -5,10 +5,23 @@
  */
 package mainescuela;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author joapi
  */
 public class Profesor {
-    
+    private int matricula;
+    public String nombre;
+    private String correo;
+    ArrayList<Grupo> grupos;
+
+    public Profesor(int matricula, String nombre, String correo){
+        this.matricula=matricula;
+        this.nombre=nombre;
+        this.correo=correo;
+        grupos = new ArrayList<>();
+    }
+
 }
