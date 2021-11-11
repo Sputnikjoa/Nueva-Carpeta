883c42938e3b28e6957e2a7d2c3afd169d1e5adf
Sputnikjoa <joaquin.ros.sed15@gmail.com>
2021-11-11 11:44:13 -0600

    commit

--- mainescuela/src/mainescuela/Profesor.java
+++ mainescuela/src/mainescuela/Profesor.java
@@ -1,27 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package mainescuela;
 
+package mainescuela;
 import java.util.ArrayList;
 
-/**
- *
- * @author joapi
- */
 public class Profesor {
     private int matricula;
-    public String nombre;
+    private String nombre;
     private String correo;
-    ArrayList<Grupo> grupos;
+    private ArrayList<Grupo> grupos;
 
     public Profesor(int matricula, String nombre, String correo){
         this.matricula=matricula;
         this.nombre=nombre;
         this.correo=correo;
         grupos = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Grupo> getGrupos() {
+        return grupos;
+    }
+
+    public void setGrupos(ArrayList<Grupo> grupos) {
+        this.grupos = grupos;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
     }
 
 }
